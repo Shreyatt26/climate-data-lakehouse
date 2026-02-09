32d1eb11f35b911f72bbe16d5b9dea7c6cecb045
Shreya Tanguturi <shreyatanguturi@Mac.lan>
2026-02-08 19:40:58 -0500

    Removed cached files

--- dbt/climate_dbt/models/example/staging/stg_climate_daily.sql
+++
@@ -1,1 +0,0 @@
-select cast(date as date) as date, location_id, location_name, lat, lon, t_mean_c, precip_mm, data_source from {{ ref('stg_climate_daily')}}
--- dbt/climate_dbt/models/marts/fact_climate_daily.sql
+++
@@ -1,2 +0,0 @@
-select date, location_id, avg(t_mean_c) as avg_temp_c, sum(precip_mm) as total_precip_mm from {{ ref('stg_climate_daily') }}
-group by 1, 2
